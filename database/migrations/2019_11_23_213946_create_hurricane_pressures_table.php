8c86b0d24367340fe4289b628b10b5c861e56424
I'm Luis! <malbertoa_11@hotmail.com>
2019-11-23 17:40:17 -0400

    add pressures migration

---
+++ database/migrations/2019_11_23_213946_create_hurricane_pressures_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHurricanePressuresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hurricane_pressures', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->integer('hurricane_id');
+            $table->integer('position_id');
+
+            $table->float('measurement');
+
+            $table->dateTime('moment');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hurricane_pressures');
+    }
+}
